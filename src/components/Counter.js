c3c0d42c4e0f1e4f6c53616ed39885e240855afa
Patryk <patrykkowalski0617@gmail.com>
2019-05-17 15:01:09 +0200

    Counter - class instead function

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,55 +1,58 @@
-const Counter = function (callback, tempo, meter, startDelay) {
-    const tt = this;
-    this.tempo = tempo;
-    this.time = function (tempo) { return 60 / tempo * 1000 };
-    this.startDelay = startDelay;
-    this.setNewTempo = function (newTempo) {
-        tt.tempo = newTempo;
-        if (tt.clock.isRun) {
-            tt.stop();
-            tt.start();
-        }
+class Counter {
+    constructor(callback, tempo, meter, startDelay) {
+        const tt = this;
 
-        return newTempo;
-    };
-    this.clock = {
-        knock: 0, 				// knocks are like seconds in regural clock
-        knocksPerBar: meter,	//git how many knocks is needed to increment bar
-        bar: 0,					// bars are like minutes in regural clock
-        barPerBarSet: 1,		// how many bars is needed to increment barSet
-        barSet: 0,				// barSets are like hours in regural clock
-        reset: function () {
-            this.knock = 0;
-            this.bar = 0;
-            this.barSet = 0;
-        },
-        isRun: 0
-    };
-    this.start = function () {
-        const time = tt.time(tt.tempo);
-        const delay = setTimeout(function () {
-            callback();
-            tt.clock.isRun = setInterval(function () {
-                tt.clock.knock++
-                if (tt.clock.knock % tt.clock.knocksPerBar === 0) {
-                    tt.clock.bar++;
-                    if (tt.clock.bar % tt.clock.barPerBarSet === 0) {
-                        tt.clock.barSet++;
-                    }
-                }
+        this.tempo = tempo;
+        this.time = function (tempo) { return 60 / tempo * 1000 };
+        this.startDelay = startDelay;
+        this.setNewTempo = function (newTempo) {
+            tt.tempo = newTempo;
+            if (tt.clock.isRun) {
+                tt.stop();
+                tt.start();
+            }
+
+            return newTempo;
+        };
+        this.clock = {
+            knock: 0,               // knocks are like seconds in regural clock
+            knocksPerBar: meter,    // git how many knocks is needed to increment bar
+            bar: 0,                 // bars are like minutes in regural clock
+            barPerBarSet: 1,        // how many bars is needed to increment barSet
+            barSet: 0,              // barSets are like hours in regural clock
+            reset: function () {
+                this.knock = 0;
+                this.bar = 0;
+                this.barSet = 0;
+            },
+            isRun: 0
+        };
+        this.start = function () {
+            const time = tt.time(tt.tempo);
+            const delay = setTimeout(function () {
                 callback();
-            }, time)
-            clearTimeout(delay)
-        }, tt.startDelay)
+                tt.clock.isRun = setInterval(function () {
+                    tt.clock.knock++
+                    if (tt.clock.knock % tt.clock.knocksPerBar === 0) {
+                        tt.clock.bar++;
+                        if (tt.clock.bar % tt.clock.barPerBarSet === 0) {
+                            tt.clock.barSet++;
+                        }
+                    }
+                    callback();
+                }, time)
+                clearTimeout(delay)
+            }, tt.startDelay)
 
-        return tt.clock.isRun;
-    };
-    this.stop = function () {
-        clearInterval(tt.clock.isRun)
-        tt.clock.isRun = 0;
+            return tt.clock.isRun;
+        };
+        this.stop = function () {
+            clearInterval(tt.clock.isRun)
+            tt.clock.isRun = 0;
 
-        return tt.clock.isRun;
-    };
+            return tt.clock.isRun;
+        };
+    }
 }
 
 const counter = new Counter(function () {
