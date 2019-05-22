51edfe274f977bfaa2c667e36753dac662a26f81
Patryk <patrykkowalski0617@gmail.com>
2019-05-22 19:29:09 +0200

    App.css instead index.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,20 +3,20 @@ import React, { Component } from "react";
 import ButtonContainer from "./buttons/Button_Container";
 
 class Header extends Component {
-  render() {
-    return (
-      <header id="header">
-        <div className="container">
-          <h1>
-            Song
-            <br />
-            Book
-          </h1>
-          <ButtonContainer />
-        </div>
-      </header>
-    );
-  }
+   render() {
+      return (
+         <header className="header">
+            <div className="container">
+               <h1>
+                  Song
+                  <br />
+                  Book
+               </h1>
+               <ButtonContainer />
+            </div>
+         </header>
+      );
+   }
 }
 
 export default Header;
