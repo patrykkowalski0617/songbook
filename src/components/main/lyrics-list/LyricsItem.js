ef1611c2c62ef157f1a89e93066041f1b823f86a
Patryk <patrykkowalski0617@gmail.com>
2019-06-04 07:01:55 +0200

    margin-left: 0 for .header-buttons

--- src/components/main/lyrics-list/LyricsItem.js
+++ src/components/main/lyrics-list/LyricsItem.js
@@ -1,17 +1,17 @@
 import React from "react";
 
 function LyricsItem(props) {
-  const style = {
-    background: "none",
-    border: "none",
-    margin: ".3rem",
-    cursor: "pointer"
-  };
-  return (
-    <li>
-      <button style={style}>{props.lyricsName}</button>
-    </li>
-  );
+   const style = {
+      background: "none",
+      border: "none",
+      margin: ".3rem",
+      cursor: "pointer"
+   };
+   return (
+      <li>
+         <button style={style}>{props.lyricsName}</button>
+      </li>
+   );
 }
 
 export default LyricsItem;
