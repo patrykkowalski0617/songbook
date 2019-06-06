ec04b29e9f61659d699f5f73e0c1e5824ae68fcb
Patryk <38049103+patrykkowalski0617@users.noreply.github.com>
2019-06-06 12:23:48 +0200

    move style from HeaderButton.js to _header.scss

--- src/components/header/header-buttons/HeaderButton.js
+++ src/components/header/header-buttons/HeaderButton.js
@@ -1,30 +1,17 @@
 import React from "react";
 
 const Button = props => {
-   const style = {
-      col: {
-         padding: "0 .4rem"
-      },
-      icon: {
-         fontSize: "20px",
-         lineHeight: "40px",
-         textAlign: "center",
-         cursor: "pointer"
-      }
-   };
-
-   return (
-      <div className="col" style={style.col}>
-         {props.test}
-         <button
-            className={`icon icon-${props.icon} button-${
-               props.buttonName
-            } circle-input`}
-            style={style.icon}
-            onClick={props.onClick}
-         />
-      </div>
-   );
+  return (
+    <div className="col header-button">
+      {props.test}
+      <button
+        className={`icon icon-${props.icon} button-${
+          props.buttonName
+        } circle-input`}
+        onClick={props.onClick}
+      />
+    </div>
+  );
 };
 
 export default Button;
