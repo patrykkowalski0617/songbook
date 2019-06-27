3805143f8514501dc69d2ce4088ff013cecfb7b3
Patryk <38049103+patrykkowalski0617@users.noreply.github.com>
2019-06-27 12:52:38 +0200

    scroll-animaton.js - implementing animation (to be continued)

--- src/components/main/lyrics/logic/scroll-animation.js
+++ src/components/main/lyrics/logic/scroll-animation.js
@@ -1,5 +1,16 @@
 class ScrollAnimation {
    constructor(parent, lyricsSections, markedSectionIndex) {
+      let startTime = null;
+
+      const easing = function(t, b, c, d) {
+         t /= d / 2;
+         if (t < 1) {
+            return (c / 2) * t * t + b;
+         }
+         t--;
+         return (-c / 2) * (t * (t - 2) - 1) + b;
+      };
+
       this.currentlyMarkedSection = lyricsSections[markedSectionIndex];
 
       this.updateData = function(markedSectionIndex) {
@@ -7,7 +18,23 @@ class ScrollAnimation {
       };
 
       this.anim = function() {
-         parent.scrollTo(0, 80);
+         const animation = function(currentTime) {
+            if (startTime === null) {
+               startTime = currentTime;
+            }
+
+            const timeElapsed = currentTime - startTime;
+            console.log(timeElapsed);
+            // const run = easing(timeElapsed, startPosition, distance, duration);
+
+            // window.scrollTo(0, run);
+
+            // if (timeElapsed < duration) {
+            //    requestAnimationFrame(animation);
+            // }
+         };
+
+         requestAnimationFrame(animation);
       };
    }
 }
