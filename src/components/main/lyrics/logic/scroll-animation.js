ba716f0ceb41a512460f610eb2d630b71b1405cb
Patryk <38049103+patrykkowalski0617@users.noreply.github.com>
2019-07-10 13:26:29 +0200

    scroll-animation.js - startPosition bug fix

--- src/components/main/lyrics/logic/scroll-animation.js
+++ src/components/main/lyrics/logic/scroll-animation.js
@@ -1,64 +1,72 @@
 class ScrollAnimation {
-   constructor(
-      container,
-      lyricsSections,
-      markedSectionIndex,
-      callbackOnStart,
-      callbackOnEnd
-   ) {
-      const tt = this;
+    constructor(
+        container,
+        lyricsSections,
+        markedSectionIndex,
+        callbackOnStart,
+        callbackOnEnd
+    ) {
+        const tt = this;
 
-      const easing = function(t, b, c, d) {
-         return (c * t) / d + b;
-      };
-      // http://www.gizma.com/easing
+        const easing = function(t, b, c, d) {
+            console.log("");
+            console.log(t, b, c, d);
+            console.log((c * t) / d + b);
+            return (c * t) / d + b;
+        };
+        // http://www.gizma.com/easing
 
-      this.markedSectionIndex = markedSectionIndex;
+        this.markedSectionIndex = markedSectionIndex;
 
-      this.updateData = function(markedSectionIndex) {
-         this.markedSectionIndex = markedSectionIndex;
-      };
+        this.updateData = function(markedSectionIndex) {
+            this.markedSectionIndex = markedSectionIndex;
+        };
 
-      this.anim = function(duration) {
-         let startTime = null;
-         const markedSectionIndex = tt.markedSectionIndex;
-         if (callbackOnStart) {
-            callbackOnStart();
-         }
-
-         const animation = function(currentTime) {
-            if (startTime === null) {
-               startTime = currentTime;
-            }
-
-            const target = lyricsSections[markedSectionIndex + 1];
-            const containerPosition = container.getBoundingClientRect().top;
-            const getTopDistance = function() {
-               const marginTop = window.getComputedStyle(lyricsSections[0])
-                  .marginTop;
-               return Number(marginTop.replace(/px/, ""));
-            };
-            const targetPosition =
-               target.getBoundingClientRect().top +
-               container.scrollTop -
-               containerPosition -
-               getTopDistance();
-            const timeElapsed = currentTime - startTime;
+        this.anim = function(duration) {
+            let startTime = null;
             const startPosition = container.scrollTop;
-            const distance = targetPosition - startPosition;
-            const run = easing(timeElapsed, startPosition, distance, duration);
-
-            container.scrollTo(0, run);
-
-            if (timeElapsed < duration) {
-               requestAnimationFrame(animation);
-            } else if (callbackOnEnd) {
-               callbackOnEnd();
+            const markedSectionIndex = tt.markedSectionIndex;
+            if (callbackOnStart) {
+                callbackOnStart();
             }
-         };
-         requestAnimationFrame(animation);
-      };
-   }
+            const animation = function(currentTime) {
+                if (startTime === null) {
+                    startTime = currentTime;
+                }
+
+                const target = lyricsSections[markedSectionIndex + 1];
+                const containerPosition = container.getBoundingClientRect().top;
+                const getTopDistance = function() {
+                    const marginTop = window.getComputedStyle(lyricsSections[0])
+                        .marginTop;
+                    return Number(marginTop.replace(/px/, ""));
+                };
+                const targetPosition =
+                    target.getBoundingClientRect().top +
+                    container.scrollTop -
+                    containerPosition -
+                    getTopDistance();
+                const timeElapsed = currentTime - startTime;
+
+                const distance = targetPosition - startPosition;
+                const run = easing(
+                    timeElapsed,
+                    startPosition,
+                    distance,
+                    duration
+                );
+
+                container.scrollTo(0, run);
+
+                if (timeElapsed < duration) {
+                    requestAnimationFrame(animation);
+                } else if (callbackOnEnd) {
+                    callbackOnEnd();
+                }
+            };
+            requestAnimationFrame(animation);
+        };
+    }
 }
 
 export default ScrollAnimation;
