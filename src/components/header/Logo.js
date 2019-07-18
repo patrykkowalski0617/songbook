15e950cc43455e3414148dcb04d0351dc8b92785
Patryk <38049103+patrykkowalski0617@users.noreply.github.com>
2019-07-18 16:55:43 +0200

    start using CSS-in-JS. Move style to components. To be continued.

--- src/components/header/Logo.js
+++ src/components/header/Logo.js
@@ -1,14 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+import { fontSize } from "../style_abstract/variables";
+
+export const H1 = styled.h1`
+    font-weight: 700;
+    font-size: ${fontSize.space4};
+    line-height: 20px;
+`;
 
 const Logo = props => {
     return !props.display ? (
-        <h1 className={`logo`}>
+        <H1 className={`logo`}>
             <a href="/">
                 Song
                 <br />
                 Book
             </a>
-        </h1>
+        </H1>
     ) : (
         ""
     );
---
+++ src/components/style_abstract/variables.js
@@ -0,0 +1,22 @@
+const fontSize = {
+    space1: `0.4rem`,
+    space2: `0.7rem`,
+    space3: `1rem`,
+    space4: `1.3rem`,
+    space5: `1.6rem`,
+    space6: `1.9rem`,
+    space7: `2.2rem`
+};
+
+const color = {
+    dark: `#000000`,
+    light: `#ffffff`
+};
+
+const variables = {
+    color: color,
+    fontSize: fontSize
+};
+
+export { fontSize, color };
+export default variables;
