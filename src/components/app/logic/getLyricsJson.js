6bcb0bcf6c951d5fa169e351737858a0d0c761cf
Patryk <38049103+patrykkowalski0617@users.noreply.github.com>
2019-07-25 17:19:46 +0200

    info about working lyrics in lyrics list moved to axios request. in case of error alert show up

--- src/components/app/logic/getLyricsJson.js
+++ src/components/app/logic/getLyricsJson.js
@@ -7,20 +7,26 @@ const getLyricsJson = function(_this) {
         const fileName = lyricsName.toLowerCase().replace(/ /g, "_");
         const filePath = `lyrics/${fileName}.${fileFormat}`;
 
-        axios.get(filePath).then(res => {
-            const lyricsData = res.data;
-            _this.counter = new Counter(res.data);
-            _this.counter.data.callbackOn.lyricsEnd = () => _this.switchIcon(1);
-            _this.setState({ lyricsData: lyricsData });
-            _this.setState({ displayPlayButton: true });
-            _this.setState({ displayLyrics: true });
-            _this.setState({
-                displayLyricsList: false,
-                displayHeaderButtons: [true, true]
-            });
-            _this.setState({ headerFocusedButtounIndex: 1 });
-            _this.switchIcon(0);
-        });
+        axios
+            .get(filePath)
+            .then(res => {
+                const lyricsData = res.data;
+                _this.counter = new Counter(res.data);
+                _this.counter.data.callbackOn.lyricsEnd = () =>
+                    _this.switchIcon(1);
+                _this.setState({ lyricsData: lyricsData });
+                _this.setState({ displayPlayButton: true });
+                _this.setState({ displayLyrics: true });
+                _this.setState({
+                    displayLyricsList: false,
+                    displayHeaderButtons: [true, true]
+                });
+                _this.setState({ headerFocusedButtounIndex: 1 });
+                _this.switchIcon(0);
+            })
+            .catch(() =>
+                alert('W tej chwili działa tylko "Kings of Leon - Sex On Fire"')
+            );
     };
 };
 export default getLyricsJson;
