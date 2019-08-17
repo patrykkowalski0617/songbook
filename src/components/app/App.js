1d85aded38c4eb3816be40c83875a66340f33ab6
Patryk <patrykkowalski0617@gmail.com>
2019-08-17 16:27:51 +0200

    section-animation.js - improve animation. forget remove test script

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -48,11 +48,6 @@ class App extends Component {
 
     searchClick = searchClick(this, this.lyricsList);
 
-    // load file for test
-    componentDidMount() {
-        this.getLyricsJson("Kings of Leon - Sex On Fire");
-    }
-
     render() {
         return (
             <div>
