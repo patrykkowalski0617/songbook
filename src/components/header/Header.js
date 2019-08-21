af194b42fc59e9629336d3b6c4a5415cba604e01
Patryk <patrykkowalski0617@gmail.com>
2019-08-21 10:41:57 +0200

    clean up: merge Logo.js with Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Logo from "./Logo";
 import Search from "./Search";
 import HeaderButtonContainer from "./header-buttons/HeaderButtonContainer";
 import styled from "styled-components";
@@ -17,7 +16,28 @@ const Container = styled.div`
     padding-bottom: ${v.headerPadding};
 `;
 
+const H1 = styled.h1`
+    font-weight: 700;
+    font-size: ${v.space.s4};
+    line-height: 20px;
+`;
+
+const Link = styled.a`
+    display: block;
+    width: 49px;
+`;
+
 const Header = function(props) {
+    const Logo = props => {
+        return !props.display ? (
+            <H1 className={`logo`}>
+                <Link href="/">Song Book</Link>
+            </H1>
+        ) : (
+            ""
+        );
+    };
+
     return (
         <HeaderElement className="header">
             <Container className="container">
