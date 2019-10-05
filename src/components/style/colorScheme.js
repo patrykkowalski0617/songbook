ed4eb65a997e049fee44bfa6587990c39c764f59
Patryk <patrykkowalski0617@gmail.com>
2019-10-06 00:33:01 +0200

    new color scheme: guitar reggae

--- src/components/style/colorScheme.js
+++ src/components/style/colorScheme.js
@@ -1,50 +1,52 @@
 export const colorScheme = [
     {
         name: "Instruments",
-
         bgImgName: "instruments",
-
         dark1: "#0D0000", // darker
         dark1Fade: "rgba(13,0,0,0.85)", // dark1 with alpha
         dark2: "#260101", // lighter
-
+        // light cannot comes from contrast. the best if it comes from secondary
         light1: "#F2F1DF", // lighter
         light2: "#CCCBBC", // darker
-
         muted: "#A6A598", // darker than textColor
-
         // contrast to the restof the colors
         contrast1: "#02593A", // darker
         contrast2: "#038054", // lighter
-
         primary1: "#A62F14", // darker
         primary2: "#CC3918", // lighter
-
         secondary1: "#591902", // darker
         sceondary2: "#802403" // lighter
     },
     {
         name: "Music Tape",
-
         bgImgName: "music_tape",
-
-        dark1: "#0A060D", // darker
-        dark1Fade: "rgba(10,6,13,0.85)", // dark1 with alpha
-        dark2: "#271A33", // lighter
-
-        light1: "#FFA082", // lighter
-        light2: "#D9876F", // darker
-
-        muted: "#B36F5B", // darker than textColor
-
-        // contrast to the restof the colors
-        contrast1: "#D9C14A", // darker
-        contrast2: "#FFE357", // lighter
-
-        primary1: "#D92949", // darker
-        primary2: "#FF3056", // lighter
-
-        secondary1: "#FF4545", // darker
-        sceondary2: "#D93B3B" // lighter
+        dark1: "#0A060D",
+        dark1Fade: "rgba(10,6,13,0.85)",
+        dark2: "#271A33",
+        light1: "#FFA082",
+        light2: "#D9876F",
+        muted: "#B36F5B",
+        contrast1: "#D9C14A",
+        contrast2: "#FFE357",
+        primary1: "#D92949",
+        primary2: "#FF3056",
+        secondary1: "#FF4545",
+        sceondary2: "#D93B3B"
+    },
+    {
+        name: "Reggae Guitar",
+        bgImgName: "reggae_guitar",
+        dark1: "#000000",
+        dark1Fade: "rgba(0,0,0,0.85)",
+        dark2: "#162612",
+        light1: "#FFFFF6",
+        light2: "#D9D9D0",
+        muted: "#C9C9C1",
+        contrast1: "#77A656",
+        contrast2: "#93CC6A",
+        primary1: "#A6170A",
+        primary2: "#CC1C0C",
+        secondary1: "#BF5B04",
+        sceondary2: "#E66E05"
     }
 ];
--- src/redux/reducers/colorSchemeNoUpdateReducer.js
+++ src/redux/reducers/colorSchemeNoUpdateReducer.js
@@ -1,11 +1,11 @@
 import { COLOR_SCHEME_NO_UPDATE } from "../actions/types";
 
-const colorSchemeNoUpdateReducer = (state = 0, action) => {
+const colorSchemeNoUpdateReducer = (state = 2, action) => {
     switch (action.type) {
         case COLOR_SCHEME_NO_UPDATE:
             return (state = action.payload);
         default:
-            return (state = 0);
+            return (state = 2);
     }
 };
 
